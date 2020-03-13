e62971839affd028aadb1ae7223dc277964385be
OneHandKlap <45212899+OneHandKlap@users.noreply.github.com>
2020-03-13 18:27:46 -0400

    added comments, and grouped like functions together

--- Poker.hs
+++ Poker.hs
@@ -6,15 +6,21 @@ import Data.List
 import Data.Map as Map
 
 pokerHands = [(0,"None"),(1,"Pair"),(2,"Two Pair"),(3,"Three of a Kind"),(4,"Straight"),(5,"Flush"),(6,"Full House"),(7,"Four of a Kind"),(8,"Straight Flush")]
-handNoSuit list  = [((x-1) `mod` 13)+1 | x<-list]
-handToSuit list = [(x-1) `div` 13 | x<-list]
-compareFirstTwo x1 x2 = x2 - x1 ==1
+
+--Custom hand data type
 
 data Hand = Hand {pokerNums :: [Int]
                 , readableHand :: [String]
                 , handVal :: Int
                 , handValName :: String
                 } deriving (Eq, Show, Read)
+
+--Generic helper functions
+
+occurencesUnique num list = [x| x<- unique list, x==num]
+occurences num list = [x| x<- list, x==num]
+
+compareFirstTwo x1 x2 = x2 - x1 ==1
 
 has [] _ = False
 has (x:xs) a
@@ -27,7 +33,54 @@ unique (x:xs)
   | otherwise = x : unique xs
 
 listToTup list = (list,list)
+
+--Helper functions for working with poker hands
+handNoSuit list  = [((x-1) `mod` 13)+1 | x<-list]
+
+handToSuit list = [(x-1) `div` 13 | x<-list]
+
 getReadableHand hand = [show (((x-1) `mod` 13)+1) ++ ["C","D","H","S","S"] !! ((x-1) `div` 13) | x<- (sortBetter hand [])]
+
+sortPair originalHand hand acc
+    | hand ==[] = acc
+    | otherwise = sortPair originalHand (tail hand) (injectPair (head hand) originalHand acc)
+
+injectPair :: Int -> [Int] -> [Int] -> [Int]
+injectPair x hand [] = [x] 
+injectPair x hand (y:ys) = if (length (occurences x hand)) <= length((occurences y hand))
+                            then y:x:ys
+                            else x : (injectPair y hand ys)
+
+getHandVal hand
+    | (detectStraight (handNoSuit hand) == True)&& (detectFlush hand False 3 == True)=8
+    | detectFlush hand False 3 == True = 5
+    | detectStraight (handNoSuit hand)  == True = 4
+    | otherwise = detectPairs (getPairs (handNoSuit hand) [])
+
+separateHands list = ([snd x |x<-zip [0..9] list, (fst x)`mod`2==1 ]) : ([snd y| y<- zip [0..9] list, (fst y)`mod`2==0]) : []
+
+getHandValName num = snd(pokerHands !! num)
+
+makeHands list = (Hand {pokerNums=(sortBetter ((separateHands list) !! 0) []) 
+, readableHand= getReadableHand (sortBetter((separateHands list) !! 0)[])
+, handVal = getHandVal ((separateHands list) !! 0)
+, handValName = getHandValName (getHandVal ((separateHands list) !! 0))}) : (Hand {pokerNums=(sortBetter ((separateHands list) !! 1) [])
+, readableHand= getReadableHand (sortBetter((separateHands list) !! 1)[]), handVal = getHandVal ((separateHands list) !! 1)
+, handValName = getHandValName (getHandVal ((separateHands list) !! 1))}) :[]
+
+inject :: Int -> [Int] -> [Int]
+inject x [] = [x] 
+inject x (y:ys) = if ((x-1) `mod` 13)+1<= ((y-1) `mod`13)+1
+                  then x:y:ys
+                  else y : inject x ys
+
+fromJust :: Maybe a -> a
+fromJust (Just a) = a
+sortBetter list acc
+    | list == [] = acc
+    | otherwise = sortBetter (tail list) (inject (head list) acc)
+
+--Detection functions
 
 detectStraight hand = isStraight (sort(handNoSuit hand)) True
 
@@ -46,9 +99,6 @@ isFlush list num
     | length [x | x<- handToSuit list, x==num] == 5 = True
     | otherwise = False
 
-occurencesUnique num list = [x| x<- unique list, x==num]
-occurences num list = [x| x<- list, x==num]
-
 getPairs (x:xs) acc
     | xs==[] = acc
     | otherwise = getPairs xs (acc++ (occurencesUnique x xs))
@@ -60,14 +110,7 @@ detectPairs list
     | (length list == 3) && length (unique list)==1 = 7
     | otherwise = 0
 
-injectPair :: Int -> [Int] -> [Int] -> [Int]
-injectPair x hand [] = [x] 
-injectPair x hand (y:ys) = if (length (occurences x hand)) <= length((occurences y hand))
-                            then y:x:ys
-                            else x : (injectPair y hand ys)
-sortPair originalHand hand acc
-    | hand ==[] = acc
-    | otherwise = sortPair originalHand (tail hand) (injectPair (head hand) originalHand acc)
+--Comparison functions
 
 compareHighCards tup1 tup2
     | head (handNoSuit(snd tup1)) == head (handNoSuit((snd tup2))) && length(snd tup1)==1 =[]
@@ -85,18 +128,6 @@ compareHighSuits tup1 tup2
     | head ((snd tup1)) > head ((snd tup2)) = fst tup1
     | head ((snd tup2)) > head ((snd tup1)) = fst tup2
 
-getHandVal hand
-    | (detectStraight (handNoSuit hand) == True)&& (detectFlush hand False 3 == True)=8
-    | detectFlush hand False 3 == True = 5
-    | detectStraight (handNoSuit hand)  == True = 4
-    | otherwise = detectPairs (getPairs (handNoSuit hand) [])
-
-separateHands list = ([snd x |x<-zip [0..9] list, (fst x)`mod`2==1 ]) : ([snd y| y<- zip [0..9] list, (fst y)`mod`2==0]) : []
-
-getHandValName num = snd(pokerHands !! num)
-
-makeHands list = (Hand {pokerNums=(sortBetter ((separateHands list) !! 0) []) , readableHand= getReadableHand (sortBetter((separateHands list) !! 0)[]), handVal = getHandVal ((separateHands list) !! 0), handValName = getHandValName (getHandVal ((separateHands list) !! 0))}) : (Hand {pokerNums=(sortBetter ((separateHands list) !! 1) []), readableHand= getReadableHand (sortBetter((separateHands list) !! 1)[]), handVal = getHandVal ((separateHands list) !! 1), handValName = getHandValName (getHandVal ((separateHands list) !! 1))}) :[]
-
 compareStraights hand1 hand2
     | (((last hand1)-1)`mod`13)+1 > (((last hand2)-1)`mod`13)+1 = hand1
     | (((last hand2)-1)`mod`13)+1 > (((last hand1)-1)`mod`13)+1 = hand2
@@ -110,24 +141,11 @@ compareHands listOfHands
     | (compareHighCards (listToTup ((pokerNums (listOfHands !! 0)))) (listToTup ((pokerNums (listOfHands !! 1))))) /= [] = getReadableHand(compareHighCards (listToTup ((pokerNums (listOfHands !! 0)))) (listToTup ((pokerNums (listOfHands !! 1)))))
     | otherwise = getReadableHand (compareHighSuits (listToTup (reverse(sortBetter (pokerNums (listOfHands !! 0)) []))) (listToTup (reverse(sortBetter (pokerNums (listOfHands !! 1)) []))))
 
-
-
+--Main function
 deal tenCards = compareHands(makeHands tenCards)
 
-inject :: Int -> [Int] -> [Int]
-inject x [] = [x] 
-inject x (y:ys) = if ((x-1) `mod` 13)+1<= ((y-1) `mod`13)+1
-                  then x:y:ys
-                  else y : inject x ys
 
-fromJust :: Maybe a -> a
-fromJust (Just a) = a
-sortBetter list acc
-    | list == [] = acc
-    | otherwise = sortBetter (tail list) (inject (head list) acc)
-
-
-
+--Test Cases
 main = do
 
     putStrLn ("Result: "++show(deal [1, 40, 10, 49, 11, 50, 12, 51, 13, 52])++ " Correct Result: "++show(["1S", "10S", "11S", "12S", "13S"]))
